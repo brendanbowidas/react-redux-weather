29ba89f246d4cae61b9182d7716c7b43932072cf
Brendan Bowidas <brendanbowidas@gmail.com>
2016-01-23 16:41:29 -0500

    added charts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,26 @@
-
-
+import axios from 'axios';
 const API_KEY="ba2b50581d781f9cfaa1a011f6bff9aa";
+const ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast?";
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const REMOVE_WEATHER = 'REMOVE_WEATHER';
+
+
+export function fetchWeather(city) {
+
+const url = ROOT_URL + "q=" + city + ",us&appid=" + API_KEY
+
+const request =  axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
+
+export function removeWeather(city) {
+
+  return {
+    type: REMOVE_WEATHER,
+    city: city
+  }
+}
--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+ class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -9,29 +12,43 @@ export default class SearchBar extends Component {
   }
 
   _onInputChange(e) {
-    console.log(e.target.value);
     this.setState({
       term: e.target.value
     })
   }
   _onFormSubmit(e) {
     e.preventDefault();
+    this.props.fetchWeather(this.state.term);
+
+    this.setState({term: ''});
   }
+
   render() {
     return (
-      <form onSubmit={(e) => this._onFormSubmit(e)} className="input-group">
-        <input type="text"
-          className="form-control"
-          value={this.state.term}
+      <div>
+        <form onSubmit={(e) => this._onFormSubmit(e)} className="input-group">
+          <input type="text"
+            className="form-control"
+            value={this.state.term}
           onChange={(e) => this._onInputChange(e)}
-        />
+          />
 
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">
+          <span className="input-group-btn">
+            <button type="submit" className="btn btn-secondary">
             Submit
-          </button>
-        </span>
-      </form>
+            </button>
+          </span>
+
+        </form>
+        </div>
     );
   }
 }
+
+
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather: fetchWeather}, dispatch);
+}
+//null = don't need state
+export default connect(null, mapDispatchToProps)(SearchBar);
